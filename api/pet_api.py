d2158a6530a9a3bf569deacb7f8a563dbc3810e6
Pavel <pashabugakov@yandex.ru>
2026-01-09 20:18:17 +0300

    Cleanup project

--- api/pet_api.py
+++
@@ -1,8 +0,0 @@
-from api.base_api import BaseAPI
-
-class PetAPI(BaseAPI):
-    def __init__(self, base_url: str):
-        super().__init__(base_url)
-
-    def create_pet(self, pet_data: dict):
-        return self._request("POST", "pet", json=pet_data)
